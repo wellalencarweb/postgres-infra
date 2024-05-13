950b7864ca7da57cb0e9be0d03c397d24d6b0b71
wellalencarweb <wellingtonlogia@gmail.com>
2024-05-12 22:46:30 -0300

    feat: :zap: infra postgres fase 03

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# postgres-infra
+## Postgres Infra
+Este repositório foi baseado no [tutorial](https://developer.hashicorp.com/terraform/tutorials/aws/aws-rds), com adaptações realizadas para permitir o deploy utilizando GitHub Actions.
+
+Repositório Terraform desenvolvido para gerenciar a infraestrutura do banco de dados PostgreSQL no âmbito do projeto Tech Challenge. O deploy do banco de dados será realizado no AWS RDS (Amazon Relational Database Service).
---
+++ outputs.tf
@@ -0,0 +1,20 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+output "rds_hostname" {
+  description = "RDS instance hostname"
+  value       = aws_db_instance.fast_food.address
+  sensitive   = true
+}
+
+output "rds_port" {
+  description = "RDS instance port"
+  value       = aws_db_instance.fast_food.port
+  sensitive   = true
+}
+
+output "rds_username" {
+  description = "RDS instance root username"
+  value       = aws_db_instance.fast_food.username
+  sensitive   = true
+}
